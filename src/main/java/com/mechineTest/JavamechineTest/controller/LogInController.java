7564a7bd3c62f78751e4bbcaea1583cebd5e7a8a
akhil babu <akhilbabu92@gmail.com>
2018-08-08 23:15:25 +0530

    commented the dependency of as of now added h2 database dependency Added welcome file config

--- src/main/java/com/mechineTest/JavamechineTest/controller/LogInController.java
+++ src/main/java/com/mechineTest/JavamechineTest/controller/LogInController.java
@@ -3,10 +3,19 @@
  */
 package com.mechineTest.JavamechineTest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author akhil
  *
  */
+@Controller
 public class LogInController {
+
+	@RequestMapping(value = "/")
+	public String logIn() {
+		return "logIn";
+	}
 
 }
